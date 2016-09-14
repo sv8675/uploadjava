6abd9689b0d9cb12407fb8de521e26300e220380
sv8675 <sv8675@att.com>
2016-09-14 09:35:34 -0500

    Initial Check in TestJavaUpload

---
+++ TestJavaUpload/src/com/att/code/HelloGitHub.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package com.att.code;
+
+/**
+ * @author sv8675
+ *
+ */
+public class HelloGitHub {
+
+	/**
+	 * 
+	 */
+	public HelloGitHub() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Sample GitHub");
+	}
+
+}
---
+++ TestJavaUpload/src/com/att/code/HelloJavaWorld.java
@@ -0,0 +1,10 @@
+package com.att.code;
+public class HelloJavaWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hope this works");
+
+	}
+
+}
